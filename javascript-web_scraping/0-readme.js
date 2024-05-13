2df27938b25c8c6fffc6d7c4d9e38bcdb757d6e6
Khadaassi <aassikhadija@yahoo.fr>
2024-05-13 09:54:35 +0200

    chore: Refactor file reading functionality

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,13 +1,22 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+const fs = require('fs').promises;
+const { exit } = require('process');
+
+async function readFileContent(filePath) {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    console.log(data);
+  } catch (err) {
+    console.error(`Error reading file: ${err.message}`);
+    exit(1);
+  }
+}
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node read-file.js <file-path>');
+  exit(1);
+}
 
 const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log(data);
-  }
-});
+readFileContent(filePath);
