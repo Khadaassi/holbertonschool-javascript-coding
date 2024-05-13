e2e3b5a0be00ba9f0f5b59e2f2ce661f8924f747
Khadaassi <aassikhadija@yahoo.fr>
2024-05-13 09:44:23 +0200

    Add file reading functionality

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+import { readFile } from 'fs';
+const filePath = process.argv[2];
+
+readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
