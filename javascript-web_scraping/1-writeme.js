185cdbfadbb17aac04168440041d9b99653a5716
Khadaassi <aassikhadija@yahoo.fr>
2024-05-13 22:01:57 +0200

    chore: Update file writing functionality to use provided string

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -3,17 +3,10 @@
 const fs = require('fs');
 
 const filePath = process.argv[2];
-const content = process.argv[3];
+const stringToWrite = process.argv[3];
 
-if (!filePath || !content) {
-  console.error('Usage: ./script.js <file_path> <content>');
-  process.exit(1);
-}
-
-fs.writeFile(filePath, content, 'utf-8', (err) => {
+fs.writeFile(filePath, stringToWrite, 'utf-8', (err) => {
   if (err) {
-    console.error(`Error writing to file: ${err.message}`);
-  } else {
-    console.log(`Content has been written to ${filePath}`);
+    console.error(err);
   }
 });
