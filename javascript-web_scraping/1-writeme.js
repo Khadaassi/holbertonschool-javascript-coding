12087b51b971f9d23faf3dae05f473ef34fdfb27
Khadaassi <aassikhadija@yahoo.fr>
2024-05-13 22:01:17 +0200

    chore: Improve error handling in file writing functionality

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -5,9 +5,14 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
+if (!filePath || !content) {
+  console.error('Usage: ./script.js <file_path> <content>');
+  process.exit(1);
+}
+
 fs.writeFile(filePath, content, 'utf-8', (err) => {
   if (err) {
-    console.error(err);
+    console.error(`Error writing to file: ${err.message}`);
   } else {
     console.log(`Content has been written to ${filePath}`);
   }
