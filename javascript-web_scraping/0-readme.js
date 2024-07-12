eab9f719a456af4ad70277e139c93c535bcd8155
Khadaassi <aassikhadija@yahoo.fr>
2024-07-12 18:16:16 +0200

    chore: Refactor file reading functionality to use fs module consistently

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,25 +1,11 @@
 #!/usr/bin/node
 
-const fs = require('fs').promises;
-const { exit } = require('process');
+const fs = require('fs');
 
-async function readFileContent(filePath) {
-  try {
-    const data = await fs.readFile(filePath, 'utf-8');
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
     console.log(data);
-  } catch (err) {
-    console.error(`Error reading file: ${err.message}`);
-    exit(1);
   }
-}
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node read-file.js <file-path>');
-  exit(1);
-}
-
-const filePath = process.argv[2];
-readFileContent(filePath).catch((err) => {
-  console.error(`Error reading file: ${err.message}`);
-  exit(1);
 });
